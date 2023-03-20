331d5f43a91b7a1069bd768f1cccd0805989cfbd
Mayank Gupta <78555479+Mayank8081@users.noreply.github.com>
2023-03-20 23:00:08 +0530

    Add files via upload

---
+++ gamex/cards1.js
@@ -0,0 +1,164 @@
+var scenarios = [
+  {
+    hackerCard : {
+      description : "I am sending you message by using fake ID.",
+      power : 3,
+    },
+    playerCards : [
+      {
+        description : "I always reply a person whether i know him/her or not.",
+        power : 1,
+      },
+      {
+        description : "I never reply a person I don't know.",
+        power : 4,
+      },
+      {
+        description : "First i will ask him/her than i reply.",
+        power : 2,
+      }
+    ]
+  },
+  {
+    hackerCard : {
+      description : "I asked some information about your friend",
+      power : 3,
+    },
+    playerCards : [
+      {
+        description : "I will never tell him/her about my friend.",
+        power : 3,
+      },
+      {
+        description : "I will tell him/her about my freind.",
+        power : 1,
+      },
+      {
+        description : "First i will ask from him/her about my friend.",
+        power : 4,
+      }
+    ]
+  },
+  {
+    hackerCard : {
+      description : "i want you to send me money because i have all your personal information ",
+      power : 4,
+    },
+    playerCards : [
+      {
+        description : "I will send you money.",
+        power : 0,
+      },
+      {
+        description : "I will call the police.",
+        power : 4,
+      },
+      {
+        description : "I will not do anything.",
+        power : 2,
+      }
+    ]
+  },
+  {
+    hackerCard : {
+      description : "I pretend to be your friend and asked you some sensitive information",
+      power : 3,
+    },
+    playerCards : [
+      {
+        description : "I will tell him/her all information.",
+        power : 1,
+      },
+      {
+        description : "I will not tell him .",
+        power : 2,
+      },
+      {
+        description : "I will tell him personally by meeting him. ",
+        power : 4,
+      }
+    ]
+  },
+  {
+    hackerCard : {
+      description : "I will ask you to send me your private images",
+      power : 3,
+    },
+    playerCards : [
+      {
+        description : "I will never send that to anybody.",
+        power : 3,
+      },
+      {
+        description : "I will tell my parents regarding this.",
+        power : 4,
+      },
+      {
+        description : "I will keep it a secret",
+        power : 1,
+      }
+    ]
+  }, 
+
+  {
+    hackerCard : {
+      description : "I will blackmail you by using some of your older chats",
+      power : 4,
+    },
+    playerCards : [
+      {
+        description : "I will fight with him/her.",
+        power : 2,
+      },
+      {
+        description : "I will do what he want from me",
+        power : 1,
+      },
+      {
+        description : "I will file a case of cyber bullying.",
+        power : 5,
+      }
+    ]
+  }, 
+
+  // {
+  //   hackerCard : {
+  //     description : "",
+  //     power : 2,
+  //   },
+  //   playerCards : [
+  //     {
+  //       description : "I use Anti-Virus Protection & Firewall to protect my system.",
+  //       power : 4,
+  //     },
+  //     {
+  //       description : "I refused to use your USB as my system was not having Anti-Virus Protection & Firewall.",
+  //       power : 3,
+  //     },
+  //     {
+  //       description : "I used your USB as I am not afraid of my system getting corrupted.",
+  //       power : 1,
+  //     }
+  //   ]
+  // }, 
+  // {
+  //   hackerCard : {
+  //     description : "Isdfgdhfjgkhhes.",
+  //     power : 2,
+  //   },
+  //   playerCards : [
+  //     {
+  //       description : "You can't because I have turned on Automatic Updates for my operating system.",
+  //       power : 4,
+  //     },
+  //     {
+  //       description : "I use web browsers such as Chrome or Firefox that receive frequent, automatic security updates.",
+  //       power : 3,
+  //     },
+  //     {
+  //       description : "I don't update my softwares nor do I download security updates.",
+  //       power : 1,
+  //     }
+  //   ]
+  //}
+];
---
+++ gamex/cards2.js
@@ -0,0 +1,164 @@
+var scenarios = [
+  {
+    hackerCard : {
+      description : "My temperature is increasing what you will do to save me.",
+      power : 2,
+    },
+    playerCards : [
+      {
+        description : "I will don't care about this",
+        power : 1,
+      },
+      {
+        description : "I will try to reduce my carbon emission",
+        power : 4,
+      },
+      {
+        description : "No nothing is wrong in my area.",
+        power : 2,
+      }
+    ]
+  },
+  {
+    hackerCard : {
+      description : "I am getting polluted day by day what you will do to save me.",
+      power : 2,
+    },
+    playerCards : [
+      {
+        description : "No no this is not my concern.",
+        power : 1,
+      },
+      {
+        description : "My enitiative will not cause any change in the world",
+        power : 2,
+      },
+      {
+        description : "I will try my best to remove pollution.",
+        power : 4,
+      }
+    ]
+  },
+  {
+    hackerCard : {
+      description : "My ozone layer is depleting what you will do to save me?",
+      power : 3,
+    },
+    playerCards : [
+      {
+        description : "I will reduce using CFC gas.",
+        power : 4,
+      },
+      {
+        description : "Ozone layer depletion is a myth.",
+        power : 1,
+      },
+      {
+        description : "I don't care about it.",
+        power : 1,
+      }
+    ]
+  },
+  {
+    hackerCard : {
+      description : "Fresh water resources are depleting what you will do for it?",
+      power : 2,
+    },
+    playerCards : [
+      {
+        description : "I will stop wasting water.",
+        power : 4,
+      },
+      {
+        description : "I will implement rain water harvesting in my home.",
+        power : 3,
+      },
+      {
+        description : "I don't care. ",
+        power : 1,
+      }
+    ]
+  },
+  {
+    hackerCard : {
+      description : "will you throw waste in open?",
+      power : 2,
+    },
+    playerCards : [
+      {
+        description : "If i found dustbin i will throw in it and if i don't i will throw the garbage anywhere.",
+        power : 2,
+      },
+      {
+        description : "It is not our duty.",
+        power : 1,
+      },
+      {
+        description : "I will never throw garbage in open",
+        power : 4,
+      }
+    ]
+  }, 
+
+  // {
+  //   hackerCard : {
+  //     description : "wfgbdgfgyfyfgyufgyugfuf",
+  //     power : 2,
+  //   },
+  //   playerCards : [
+  //     {
+  //       description : "I follow the 3-2-1 backup rule. I have on-site as well as off-site location (cloud storage) backup.",
+  //       power : 4,
+  //     },
+  //     {
+  //       description : "I have my data backed up in local and external hard drive.",
+  //       power : 3,
+  //     },
+  //     {
+  //       description : "I never backed up my data in any way.",
+  //       power : 1,
+  //     }
+  //   ]
+  // }, 
+
+  // {
+  //   hackerCard : {
+  //     description : "sdfdghfjghkj",
+  //     power : 2,
+  //   },
+  //   playerCards : [
+  //     {
+  //       description : "I use Anti-Virus Protection & Firewall to protect my system.",
+  //       power : 4,
+  //     },
+  //     {
+  //       description : "I refused to use your USB as my system was not having Anti-Virus Protection & Firewall.",
+  //       power : 3,
+  //     },
+  //     {
+  //       description : "I used your USB as I am not afraid of my system getting corrupted.",
+  //       power : 1,
+  //     }
+  //   ]
+  // }, 
+  // {
+  //   hackerCard : {
+  //     description : "Isdfgdhfjgkhhes.",
+  //     power : 2,
+  //   },
+  //   playerCards : [
+  //     {
+  //       description : "You can't because I have turned on Automatic Updates for my operating system.",
+  //       power : 4,
+  //     },
+  //     {
+  //       description : "I use web browsers such as Chrome or Firefox that receive frequent, automatic security updates.",
+  //       power : 3,
+  //     },
+  //     {
+  //       description : "I don't update my softwares nor do I download security updates.",
+  //       power : 1,
+  //     }
+  //   ]
+  // }
+];
